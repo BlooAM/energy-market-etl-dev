7af85ae3cc126985f402376548a5873106943788
BlooAM <adam3200@vp.pl>
2023-01-17 11:47:46 +0100

    PSE extractor template

--- energy_market_etl/extractors/pse/pse_extractor.py
+++ energy_market_etl/extractors/pse/pse_extractor.py
@@ -1,5 +1,7 @@
 import datetime as dt
 
+import pandas as pd
+
 from energy_market_etl.extractors.extractor import Extractor
 from energy_market_etl.extractors.pse.utils import _PSE_DATA_TYPE_URL_MAPPER
 
@@ -12,8 +14,19 @@ class PseExtractor(Extractor):
         if self.__url_getter is None:
             raise NotImplementedError(f'data type: {data_type} not implemented') #TODO: replace with custom Exception
 
-    def extract(self):
-        pass
+    def extract(self) -> pd.DataFrame:
+        data_snapshots = []
+        for date in pd.date_range(self.start_date, self.end_date):
+            data_snapshot = self.__get_data_snapshot(date) #TODO: handle possible exceptions here
+            data_snapshots.append(data_snapshot)
 
-    def
+        return pd.concat(data_snapshots)
 
+    def __get_data_snapshot(self, date: dt.datetime) -> pd.DataFrame:
+        url = self.__url_getter(date)
+        df = pd.read_csv(
+            filepath_or_buffer=url,
+            sep=';',
+            encoding='cp1250',
+        )
+        return df
--- energy_market_etl/extractors/pse/utils.py
+++ energy_market_etl/extractors/pse/utils.py
@@ -1,3 +1,7 @@
+from typing import Tuple
+import datetime as dt
+
+
 PSE_CSV_GET_BASIC_URL = 'https://pse.pl/getcsv/-/export/csv'
 
 PSE_UNITS_GENERATION_URL_PREFIX = 'PL_GEN_MOC_JW_EPS/data'
@@ -6,12 +10,21 @@ PSE_UNITS_GENERATION_URL_SUFFIX = 'unit/all'
 PSE_SYSTEM_DATA_URL_PREFIX = 'PL_WYK_KSE/data'
 
 
-def __get_pse_unit_generation_url(year: int, month: int, day: int) -> str:
+def __extract_date_components(date: dt.datetime) -> Tuple[int]:
+    year = date.year
+    month = date.month
+    day = date.day
+    return year, month, day
+
+
+def __get_pse_unit_generation_url(date: dt.datetime) -> str:
+    year, month, day = __extract_date_components(date)
     return f'{PSE_CSV_GET_BASIC_URL}/{PSE_UNITS_GENERATION_URL_PREFIX}/{year}{month:02d}{day:02d}/' \
            f'{PSE_UNITS_GENERATION_URL_SUFFIX}'
 
 
-def __get_pse_system_data_url(year: int, month: int, day: int) -> str:
+def __get_pse_system_data_url(date: dt.datetime) -> str:
+    year, month, day = __extract_date_components(date)
     return f'{PSE_CSV_GET_BASIC_URL}/{PSE_SYSTEM_DATA_URL_PREFIX}/{year}{month:02d}{day:02d}'
 
 
