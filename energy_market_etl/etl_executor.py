1a2e6972dea057f918ab5cfaf22c247ee74006fd
BlooAM <adam3200@vp.pl>
2023-01-19 13:27:01 +0100

    delete abstract EtlExecutor

--- energy_market_etl/etl_executor.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class EtlExecutor(ABC):
-    @abstractmethod
-    def execute(self):
-        raise NotImplementedError('Executor has to implement `execute` method')
