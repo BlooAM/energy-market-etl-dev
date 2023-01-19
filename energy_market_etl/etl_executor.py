396046cc56e5611b567e3f50fd4e39d13ab30001
BlooAM <adam3200@vp.pl>
2023-01-19 23:28:38 +0100

    replace positional with keyword args in etl exec

--- energy_market_etl/etl_executor.py
+++ energy_market_etl/etl_executor.py
@@ -4,6 +4,7 @@ from typing import Any
 
 import pydantic
 
+from energy_market_etl.etls.etl import Etl
 from energy_market_etl.etls.market_data_etl import MarketDataEtl
 
 _REPORT_TYPES = [
@@ -14,39 +15,39 @@ _TODAY = dt.datetime.today()
 
 
 class FutureDateError(Exception):
-    def __int__(self, date: dt.datetime, message: str) -> None:
+    def __init__(self, date: dt.datetime, message: str) -> None:
         self.date = date
         self.message = message
         super().__init__(message=self.message)
 
 
 class ReportTypeNotImplementedError(Exception):
-    def __int__(self, report_type: dt.datetime, message: str) -> None:
+    def __init__(self, report_type: dt.datetime, message: str) -> None:
         self.report_type = report_type
         self.message = message
         super().__init__(message=self.message)
 
 
 class NonChronologicalDateOrderError(Exception):
-    def __int__(self, start_date: dt.datetime, end_date: dt.datetime, message: str) -> None:
+    def __init__(self, start_date: dt.datetime, end_date: dt.datetime, message: str) -> None:
         self.start_date = start_date
         self.end_date = end_date
         self.message = message
         super().__init__(message=self.message)
 
 
-class EtlExecutor(pydantic.BaseModel): #TODO: inherits from abstract EtlExecutor?
+class EtlExecutor(pydantic.BaseModel):
     start_date: dt.datetime
     end_date: dt.datetime
-    report_type: str #TODO: add validator for report type and mapper report_type->date_source
+    report_type: str
 
     @pydantic.validator("end_date")
     @classmethod
     def end_date_validator(cls, value: dt.datetime) -> None:
         if value > _TODAY:
             raise FutureDateError(
-                value,
-                "`end_date` cannot be later than the current date" #TODO: keyword args, not positional
+                date=value,
+                message="`end_date` cannot be later than the current date"
             )
 
     @pydantic.validator("report_type")
@@ -54,8 +55,8 @@ class EtlExecutor(pydantic.BaseModel): #TODO: inherits from abstract EtlExecutor
     def report_type_validator(cls, value: str) -> None:
         if value not in _REPORT_TYPES:
             raise ReportTypeNotImplementedError(
-                value,
-                "`end_date` cannot be later than the current date" #TODO: keyword args, not positional
+                report_type=value,
+                message="`end_date` cannot be later than the current date"
             )
 
     @pydantic.root_validator(pre=True)
@@ -64,9 +65,9 @@ class EtlExecutor(pydantic.BaseModel): #TODO: inherits from abstract EtlExecutor
         start_date, end_date = values.get('start_date'), values.get('end_date')
         if start_date > end_date:
             raise NonChronologicalDateOrderError(
-                start_date,
-                end_date,
-                "`start_date` and `end_date` must be in chronological order", #TODO: keyword args, not positional
+                start_date=start_date,
+                end_date=end_date,
+                message="`start_date` and `end_date` must be in chronological order",
             )
         return values
 
@@ -76,12 +77,13 @@ class EtlExecutor(pydantic.BaseModel): #TODO: inherits from abstract EtlExecutor
         # etl.transform() #TODO: add loggs between layers
         # etl.load() #TODO: add loggs between layers
 
-    def __get_etl(self) -> MarketDataEtl:
-        data_source = self.report_type
-        return MarketDataEtl(
+    def __get_etl(self) -> Etl:
+        data_source = self.report_type #TODO: add mapper (report_type) -> (data_source, report_name)
+        return MarketDataEtl( #TODO: add etl factory based on (data_source, report_name)
             start_date=self.start_date,
             end_date=self.end_date,
             data_source=data_source,
+            report_name=self.report_type,
         )
 
 
