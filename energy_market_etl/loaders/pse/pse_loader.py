30f7eb71fdb83842acb8ed66d77bed75708e58bd
BlooAM <adam3200@vp.pl>
2023-01-19 14:32:23 +0100

    template packages for transformers and loaders

---
+++ energy_market_etl/loaders/pse/pse_loader.py
@@ -0,0 +1,6 @@
+from energy_market_etl.loaders.loader import Loader
+
+
+class PseLoader(Loader):
+    def load(self):
+        pass
---
+++ energy_market_etl/loaders/tge/tge_loader.py
@@ -0,0 +1,6 @@
+from energy_market_etl.loaders.loader import Loader
+
+
+class TgeLoader(Loader):
+    def load(self):
+        pass
