537b6590ed344b1b289969570a6471bd10943273
BlooAM <adam3200@vp.pl>
2023-01-19 22:12:12 +0100

    date column transformer

---
+++ energy_market_etl/transformers/DateColumnTransformer/date_column_transformer.py
@@ -0,0 +1,25 @@
+import datetime as dt
+from typing import Dict
+
+import pandas as pd
+
+from energy_market_etl.transformers.transformer import Transformer
+
+
+class DateColumnTransformer(Transformer):
+    def __init__(self, date_column_name: str = 'Data'):
+        self.date_column_name = date_column_name
+
+    def transform(self, raw_data_snapshots: Dict[dt.datetime, pd.DataFrame]):
+        data_snapshots = {}
+        for datetime, raw_data_snapshot in raw_data_snapshots.items():
+            date = datetime.date()
+            data_snapshots[datetime]: pd.DataFrame = raw_data_snapshot.copy()
+            data_snapshots[datetime].insert(
+                loc=0,
+                column=self.date_column_name,
+                value=date,
+                allow_duplicates=True,
+            )
+
+        return data_snapshots
