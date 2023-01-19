feddf2b124b0c30c718943980a4c94bdcc59d3ff
BlooAM <adam3200@vp.pl>
2023-01-19 16:05:40 +0100

    remove pse,tge loader templates

--- energy_market_etl/loaders/pse/pse_loader.py
+++
@@ -1,6 +0,0 @@
-from energy_market_etl.loaders.loader import Loader
-
-
-class PseLoader(Loader):
-    def load(self):
-        pass
--- energy_market_etl/loaders/tge/tge_loader.py
+++
@@ -1,6 +0,0 @@
-from energy_market_etl.loaders.loader import Loader
-
-
-class TgeLoader(Loader):
-    def load(self):
-        pass
