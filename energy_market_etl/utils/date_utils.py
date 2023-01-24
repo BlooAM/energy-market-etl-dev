347733bb4fcd1e78265cfa7394a4a0b471a505e7
BlooAM <adam3200@vp.pl>
2023-01-24 09:36:04 +0100

    add date utils for time-shifts

--- energy_market_etl/utils/date_utils.py
+++ energy_market_etl/utils/date_utils.py
@@ -1,4 +1,5 @@
 import datetime as dt
+from dateutil.rrule import rrule, SU, WEEKLY
 import re
 from typing import Tuple
 
@@ -13,3 +14,20 @@ def extract_date_components(date: dt.datetime) -> Tuple[int, int, int]:
     return year, month, day
 
 
+def get_last_sunday(year: int, month: int):
+    date = dt.datetime(year=year, month=month, day=1)
+    days = rrule(freq=WEEKLY, dtstart=date, byweekday=SU, count=5)
+    last_rule_sunday, second_to_last_rule_sunday = days[-1], days[-2]
+    return last_rule_sunday if last_rule_sunday.month == month else second_to_last_rule_sunday
+
+
+def get_march_switch(year: int):
+    _MARCH_MONTH_NO = 3
+    day = get_last_sunday(year=year, month=_MARCH_MONTH_NO)
+    return day.replace(hour=1, minute=0, second=0, microsecond=0)
+
+
+def get_october_switch(year: int):
+    _OCTOBER_MONTH_NO = 10
+    day = get_last_sunday(year=year, month=_OCTOBER_MONTH_NO)
+    return day.replace(hour=2, minute=0, second=0, microsecond=0)
--- tests/unit/energy_market_etl/extractors/tge/test_tge_scrapper.py
+++ tests/unit/energy_market_etl/extractors/tge/test_tge_scrapper.py
@@ -5,20 +5,20 @@ from unittest.mock import MagicMock
 import pandas as pd
 import pytest
 
-from energy_market_etl.extractors.tge.tge_extractor import TgeExtractor
+from energy_market_etl.extractors.tge.tge_extractor import TgeScrapper
 from tests.unit.energy_market_etl.resources_path_getter import get_resources_path
 
 
-def get_extractor_resource_path(endpoint: str, date: dt.datetime) -> Path:
-    return get_resources_path() / 'extractors' / 'tge' / f'{endpoint}_{date.strftime("%Y%m%d")}.csv'
+def get_scrapper_resource_path(endpoint: str, date: dt.datetime) -> Path:
+    return get_resources_path() / 'extractors' / 'tge' / f'{endpoint}_{date.strftime("%Y%m%d")}.html'
 
 
-@pytest.mark.parametrize("endpoint", ['PL_GEN_MOC_JW_EPS', 'PL_WYK_KSE'])
+@pytest.mark.parametrize("endpoint", ['energia-elektryczna-rdn'])
 def test_units_extract(start_date: dt.datetime, end_date: dt.datetime, endpoint: str):
     mocked_url_factory_provider = MagicMock()
     mocked_url_factory_provider.get_url_provider.return_value = \
-        lambda date: get_extractor_resource_path(date=date, endpoint=endpoint)
-    extractor = TgeExtractor(
+        lambda date: get_scrapper_resource_path(date=date, endpoint=endpoint)
+    extractor = TgeScrapper(
         start_date=start_date,
         end_date=end_date,
         url_provider_factory=mocked_url_factory_provider
@@ -35,3 +35,9 @@ def test_units_extract(start_date: dt.datetime, end_date: dt.datetime, endpoint:
     for date in date_range:
         assert isinstance(result.get(date), pd.DataFrame)
         assert set(expected.get(date).columns) == set(result.get(date).columns)
+
+
+
+
+# with open("energia-elektryczna-rdn_20230101.html") as fp:
+#     soup = BeautifulSoup(fp, 'html.parser')
