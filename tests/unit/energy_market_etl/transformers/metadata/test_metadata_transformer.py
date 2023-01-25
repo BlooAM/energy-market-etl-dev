f810e1d37d2beece77cb90b00057fd5486447de9
BlooAM <adam3200@vp.pl>
2023-01-25 21:26:32 +0100

    add test for metadata transform

--- tests/unit/energy_market_etl/transformers/metadata/test_metadata_transformer.py
+++ tests/unit/energy_market_etl/transformers/metadata/test_metadata_transformer.py
@@ -1,2 +1,18 @@
+import pandas as pd
+
+from energy_market_etl.transformers.metadata.metadata_transformer import MetadataTransformer
+
+
 def test_transform():
-    pass
+    raw_data = pd.DataFrame(
+        {
+        ' A': [1, 2, 3],
+        'B  ': [4, 5, 6],
+        '\nC\t': ['7', '8', '9'],
+        },
+        index=[3, 2, 1]
+    )
+    transformed_data = MetadataTransformer(reset_index=True).transform(raw_data)
+
+    assert set(transformed_data.columns) == {'A', 'B', 'C'}
+    assert transformed_data.index.to_list() == [0, 1, 2]
