d334aad466e7b4bc4c45e2404c9bfc73895b4577
BlooAM <adam3200@vp.pl>
2023-02-23 10:32:12 +0100

    template code for gcs config

---
+++ energy_market_etl/loaders/google_storage/config.py
@@ -0,0 +1,2 @@
+class GoogleCloudStorageConfig:
+    pass
--- energy_market_etl/loaders/google_storage/google_cloud_storage_loader.py
+++ energy_market_etl/loaders/google_storage/google_cloud_storage_loader.py
@@ -1,5 +1,11 @@
 from energy_market_etl.loaders.loader import Loader
+from energy_market_etl.loaders.google_storage.config import GoogleCloudStorageConfig
 
 
 class GoogleCloudStorageLoader(Loader):
-    pass
+    def __init__(self, config: GoogleCloudStorageConfig):
+        client = self._create_client(config.key_path)
+        self._bucket = client.bucket(config.bucket_name)
+
+    def load(self):
+        pass
