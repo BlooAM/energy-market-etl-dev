ef4841ddf4d9244725d1130a94e621f52c712f59
BlooAM <adam3200@vp.pl>
2023-02-23 10:20:57 +0100

    basic code for gcs client provider

---
+++ energy_market_etl/loaders/google_storage/client/google_cloud_storage_client.py
@@ -0,0 +1,17 @@
+from google.cloud import storage
+
+
+class GoogleStorageKeyNotFound(Exception):
+    def __init__(self, path: str):
+        self.message = f'Google storage key has not been found for path: {path}.' \
+                       f' Please provide the correct path in the configuration file.'
+        super().__init__(self.message)
+
+
+def create_gcs_client(key_path: str) -> storage.Client:
+    if key_path is None:
+        return storage.Client()
+    else:
+        if not os.path.exists(key_path):
+            raise GoogleStorageKeyNotFound(path=key_path)
+        return storage.Client.from_service_account_json(key_path)
