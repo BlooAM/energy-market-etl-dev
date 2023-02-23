cc1c74729d4977a613b56413f8b62fb3b843bd05
BlooAM <adam3200@vp.pl>
2023-02-23 18:52:01 +0100

    template logic for gcs loader

--- energy_market_etl/loaders/google_storage/google_cloud_storage_loader.py
+++ energy_market_etl/loaders/google_storage/google_cloud_storage_loader.py
@@ -1,11 +1,13 @@
 from energy_market_etl.loaders.loader import Loader
 from energy_market_etl.loaders.google_storage.config import GoogleCloudStorageConfig
+from energy_market_etl.loaders.google_storage.client.google_cloud_storage_client import create_gcs_client
 
 
 class GoogleCloudStorageLoader(Loader):
     def __init__(self, config: GoogleCloudStorageConfig):
-        client = self._create_client(config.key_path)
+        client = create_gcs_client(key_path=config.key_path)
         self._bucket = client.bucket(config.bucket_name)
 
     def load(self):
-        pass
+        blob = self._bucket.blob('test.csv')
+        blob.upload_from_string(df.to_csv(), 'text/csv')
