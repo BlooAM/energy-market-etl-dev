acdf8e0f97546102dd3f2e46e5ccbaab85040ccc
BlooAM <adam3200@vp.pl>
2023-02-28 11:22:42 +0100

    extend load method for gcp loader

--- energy_market_etl/loaders/google_storage/google_cloud_storage_loader.py
+++ energy_market_etl/loaders/google_storage/google_cloud_storage_loader.py
@@ -4,10 +4,13 @@ from energy_market_etl.loaders.google_storage.client.google_cloud_storage_client
 
 
 class GoogleCloudStorageLoader(Loader):
-    def __init__(self, config: GoogleCloudStorageConfig):
+    def __init__(self, config: GoogleCloudStorageConfig) -> None:
         client = create_gcs_client(key_path=config.key_path)
         self._bucket = client.bucket(config.bucket_name)
 
-    def load(self):
-        blob = self._bucket.blob('test.csv')
-        blob.upload_from_string(df.to_csv(), 'text/csv')
+    def load(self, transformed_data: pd.DataFrame) -> None:
+        if transformed_data.empty:
+            logging.warning('No data to load. Omitting load phase')
+        else:
+            blob = self._bucket.blob('test.csv')
+            blob.upload_from_string(df.to_csv(), 'text/csv')
