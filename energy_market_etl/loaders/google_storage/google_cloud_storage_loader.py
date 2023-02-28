f3419dcd4776f21942ee0abfef14f6c94cd849eb
BlooAM <adam3200@vp.pl>
2023-02-28 11:37:25 +0100

    add parameters for loaded files

--- energy_market_etl/loaders/google_storage/google_cloud_storage_loader.py
+++ energy_market_etl/loaders/google_storage/google_cloud_storage_loader.py
@@ -1,12 +1,15 @@
 import logging
 
+import pandas as pd
+
 from energy_market_etl.loaders.loader import Loader
 from energy_market_etl.loaders.google_storage.config import GoogleCloudStorageConfig
 from energy_market_etl.loaders.google_storage.client.google_cloud_storage_client import create_gcs_client
 
 
 class GoogleCloudStorageLoader(Loader):
-    def __init__(self, config: GoogleCloudStorageConfig) -> None:
+    def __init__(self, config: GoogleCloudStorageConfig, file_name: str) -> None:
+        self.file_name = f'{file_name}.csv'
         client = create_gcs_client(key_path=config.key_path)
         self._bucket = client.bucket(config.bucket_name)
 
@@ -14,5 +17,10 @@ class GoogleCloudStorageLoader(Loader):
         if transformed_data.empty:
             logging.warning('No data to load. Omitting load phase')
         else:
-            blob = self._bucket.blob('test.csv')
+            blob = self._bucket.blob(f'{self.file_name}')
+            if blob.exists():
+                logging.warning("File: %s has already existed and will be overridden", self.file_name)
             blob.upload_from_string(transformed_data.to_csv(), 'text/csv')
+            if blob.exists():
+                logging.info("File: %s saved successfully to bucket: %s", self.file_name, self._bucket)
+s
