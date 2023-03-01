bc11557117d6487da6b8ec0450bad1252117a253
BlooAM <adam3200@vp.pl>
2023-03-01 13:26:23 +0100

    code for config

---
+++ energy_market_etl/config.py
@@ -0,0 +1,25 @@
+import pathlib
+from typing import Type, TypeVar
+
+import dacite
+from pydantic import BaseModel
+import yaml
+
+from energy_market_etl.loaders.google_storage.config import GoogleCloudStorageConfig
+
+
+class Config(BaseModel):
+    google_storage: GoogleCloudStorageConfig
+
+
+T = TypeVar("T")
+
+
+def load_config(config_path: pathlib.Path, config_class: Type[T] = Config) -> T:
+    with config_path.open() as f:
+        raw_config = yaml.load(f, Loader=yaml.SafeLoader)
+    if raw_config is not None:
+        return dacite.from_dict(
+            data_class=config_class,
+            data=raw_config,
+        )
